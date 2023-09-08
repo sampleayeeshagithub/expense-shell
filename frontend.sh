2238684d676f15ad3a052bd29dd2888aadd10e6f
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-08 13:16:08 +0530

    using variables

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,21 @@
-log_file=/temp/expense.log
+
 echo installing nginx
-dnf install nginx -y
+dnf install nginx -y >/tmp/expense.log
+
 echo placing expense conf file in nginix
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
 
 echo removing old nginix content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* >/tmp/expense.log
 
 echo download frontend code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp/expense.log
 
 cd /usr/share/nginx/html
 
 echo extarcting front end code
-unzip /tmp/frontend.zip >>$log_file
+unzip /tmp/frontend.zip >/tmp/expense.log
 
 echo starting nginix service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx >/tmp/expense.log
+systemctl restart nginx >/tmp/expense.log
