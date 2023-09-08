4ff0033eab2809118d8c5c336fb191aff7f04d40
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-08 13:22:28 +0530

    using double redirector to append the content

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,21 @@
 
 echo installing nginx
-dnf install nginx -y >/tmp/expense.log
+dnf install nginx -y >>/tmp/expense.log
 
 echo placing expense conf file in nginix
-cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
 
 echo removing old nginix content
-rm -rf /usr/share/nginx/html/* >/tmp/expense.log
+rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 
 echo download frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 cd /usr/share/nginx/html
 
 echo extarcting front end code
-unzip /tmp/frontend.zip >/tmp/expense.log
+unzip /tmp/frontend.zip >>/tmp/expense.log
 
 echo starting nginix service
-systemctl enable nginx >/tmp/expense.log
-systemctl restart nginx >/tmp/expense.log
+systemctl enable nginx >>/tmp/expense.log
+systemctl restart nginx >>/tmp/expense.log
