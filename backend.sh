3ba73e3c4f2dab04bd857d8c895e8ba8b13d9b41
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-09 21:47:50 +0530

    Using if condition

--- backend.sh
+++ backend.sh
@@ -7,6 +7,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32msuccess\e[0m"
 else
   echo -e "\e[31mfailure\e[0m"
+  exit
 fi
 
 echo install nodejs
@@ -15,6 +16,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32msuccess\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
+    exit
   fi
 
 echo copy backend service file
@@ -23,6 +25,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32msuccess\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
+    exit
   fi
 
 echo add application user
@@ -31,6 +34,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32msuccess\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
+    exit
   fi
 
 echo clean app content
