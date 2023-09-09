f6b2b5f0f8a8f18f655fc7bc5518a4c57574cf78
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-09 22:33:08 +0530

    Using if condition

--- backend.sh
+++ backend.sh
@@ -1,14 +1,16 @@
 source common.sh
 component=backend
 
-echo install nodejs repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-stat_check
+type npm &>>$log_file
+if [ $? -ne 0 ]; then
+ echo install nodejs repos
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+ stat_check
 
-echo install nodejs
-dnf install nodejs -y &>>$log_file
-stat_check
-
+ echo install nodejs
+ dnf install nodejs -y &>>$log_file
+ stat_check
+fi
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 stat_check
@@ -22,7 +24,7 @@ stat_check
 
 echo clean app content
 rm -rf /app &>>$log_file
-echo $?
+stat_check
 mkdir /app
 cd /app
 
@@ -30,18 +32,18 @@ download_and_extract
 
 echo download dependencies
 npm install &>>$log_file
-echo $?
+stat_check
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-echo $?
+stat_check
 
 echo install mysql client
 dnf install mysql -y &>>$log_file
-echo $?
+stat_check
 
 echo load schema
 mysql -h mysql.ayeeshadevops75.online	-uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-echo $?
+stat_check
